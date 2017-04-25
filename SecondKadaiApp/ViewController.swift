000c32ec4ce0011f6acc7c80d70ee8defafcfec6
高橋 陽介 <YosukeTakahashi@Yosuke-Takahashi-no-MacBook-Air.local>
2017-04-26 00:14:56 +0900

    あいさつアプリの作成。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.x = textfield.text
+    }
 
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        
+    }
 
 }
 
